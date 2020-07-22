94e3670c55dda084b9c50bf41f890f5ae2486674
Marc Grau Carrión <negu.marc@gmail.com>
2020-07-22 18:55:58 +0200

    End of Section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -7,10 +7,8 @@ configure({enforceActions: 'always'});
 
 class ActivityStore {
     @observable activityRegistry = new Map();
-    @observable activities: IActivity[] = [];
     @observable loadingInitial = false;
-    @observable selectedActivity: IActivity | undefined;
-    @observable editMode: boolean = false;
+    @observable activity: IActivity | null = null;
     @observable submitting: boolean = false;
     @observable target: string = '';
 
@@ -38,13 +36,41 @@ class ActivityStore {
         }
     }
 
+    @action loadActivity = async (id: string) => {
+        let activity = this.getActivity(id)
+        if (activity) {
+            this.activity = activity;
+        } else {
+            this.loadingInitial = true;
+            try {
+                activity = await agent.Activities.details(id);
+                runInAction('getting activity', () => {
+                    this.activity = activity;
+                    this.loadingInitial = false;
+                })
+            } catch (error) {
+                runInAction('get activity error', () => {
+                    this.loadingInitial = false;
+                })
+                console.log(error);
+            }
+        }
+    }
+
+    @action clearActivity = () => {
+        this.activity = null;
+    }
+
+    getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    }
+
     @action createActivity = async (activity: IActivity) => {
         this.submitting = true;
         try {
             await agent.Activities.create(activity);
             runInAction('creating activity', () => {
                 this.activityRegistry.set(activity.id, activity);
-                this.editMode= false;
                 this.submitting = false;
             })
         }
@@ -62,8 +88,7 @@ class ActivityStore {
             await agent.Activities.update(activity);
             runInAction('editing activity', () => {
                 this.activityRegistry.set(activity.id, activity);
-                this.selectedActivity = activity;
-                this.editMode= false;
+                this.activity = activity;
                 this.submitting = false;
             })
         }
@@ -93,29 +118,6 @@ class ActivityStore {
             })
             console.log(error);
         }  
-    }
-
-    @action openCreateForm = () => {
-        this.selectedActivity = undefined;
-        this.editMode = true;
-    }
-
-    @action openEditForm = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-        this.editMode = true;
-    }
-
-    @action cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    @action cancelFormOpen = () => {
-        this.editMode = false;
-    }
-
-    @action selectActivity = (id: string)  => {
-        this.selectedActivity = this.activityRegistry.get(id);
-        this.editMode = false;
     }
 }
 
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,44 +1,48 @@
-import React, {useState, FormEvent, useContext} from 'react'
+import React, {useState, FormEvent, useContext, useEffect} from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 import {v4 as uuid} from 'uuid';
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore';
+import { RouteComponentProps } from 'react-router';
 
-interface IProps {
-    activity: IActivity | undefined;   
+interface DetailParams {
+    id: string;
 }
 
-const ActivityForm: React.FC<IProps> = ({activity: initialFormState}) => {
-    const activityStore = useContext(ActivityStore)
-    const { createActivity, editActivity, submitting, cancelFormOpen } = activityStore;
-    const initializeForm = () => {
-        if (initialFormState) {
-            return initialFormState
-        } else {
-            return {
-                id: '',
-                title: '',
-                category: '',
-                description: '',
-                date: '',
-                city: '',
-                venue: ''
-            }
-        }
-    };
-
-    const [activity, setActivity] = useState<IActivity>(initializeForm)
+const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, history}) => {
+    const activityStore = useContext(ActivityStore);
+    const { createActivity, editActivity, submitting, activity: initialFormState, loadActivity, clearActivity } = activityStore;
     
+    const [activity, setActivity] = useState<IActivity>({
+        id: "",
+        title: "",
+        category: "",
+        description: "",
+        date: "",
+        city: "",
+        venue: ""
+      });
+    
+      useEffect(() => {
+        if (match.params.id && activity.id.length === 0) {
+            loadActivity(match.params.id).then(() => initialFormState && setActivity(initialFormState));
+        }
+        return (() => {
+            clearActivity();
+        });
+    }, [loadActivity, match.params.id, clearActivity, initialFormState, activity.id.length]);
+
+
     const handleSubmit = () => {
         if (activity.id.length === 0) {
             let newActivity = {
                 ...activity,
                 id: uuid()
             }
-            createActivity(newActivity);
+            createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
         } else {
-            editActivity(activity);
+            editActivity(activity).then(() => history.push(`/activities/${activity.id}`));
         }
     }; 
 
@@ -57,7 +61,7 @@ const ActivityForm: React.FC<IProps> = ({activity: initialFormState}) => {
                 <Form.Input onChange={handleInputChange} name='city' placeholder='City'  value={activity.city}  />
                 <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue}  />
                 <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
-                <Button onClick={cancelFormOpen} floated='right' type='button' content='Cancel' />
+                <Button onClick={() => history.push('/activities')} floated='right' type='button' content='Cancel' />
             </Form>            
         </Segment>
     )
